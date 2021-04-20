ab85c1086c91d29a53e6adf51e9a019f93b6e529
diego-romero <dev.diego.romero@gmail.com>
2021-04-20 17:38:10 -0600

    Fixed button on landing page when user is logged in

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,11 +2,12 @@ import { VStack, Heading, Text, Button, Image } from "@chakra-ui/react";
 import React from "react";
 import { useHistory } from "react-router";
 import { config } from "../config";
+import { useAuthenticatedContext } from "../context/AuthenticatedContext";
 import logo from "../images/icons/undraw_experience_design_eq3j.svg";
 
 export const Landing: React.FC = () => {
   const history = useHistory();
-  const isAuthenticated = false;
+  const { user } = useAuthenticatedContext()
   return (
     <VStack spacing={6} mt={8} textAlign="center">
       <Heading as="h1" size="2xl">
@@ -15,7 +16,7 @@ export const Landing: React.FC = () => {
       <Text fontSize="xl">
         The app to help you and your friends share lists.
       </Text>
-      {isAuthenticated ? (
+      {user !== null ? (
         <Button
           variant="outline"
           colorScheme={"yellow"}
